633a039e8ff3e86f8081538a0f316a22bdcc628c
Jeremy John <jmjohn@riseup.net>
2020-04-04 13:56:44 -0500

    fix(build): move from yarn to npm to fix cache error

--- scripts/yarn-link-and-yalc-publish-theme.sh
+++ scripts/yarn-link-and-yalc-publish-theme.sh
@@ -6,8 +6,8 @@ THEME_REPO_RELATIVE_PATH="src/themes/$THEME_NAME"
 YALC_BIN_PATH="node_modules/.bin/yalc"
 
 cd $ROOT/$THEME_REPO_RELATIVE_PATH
-yarn link
+npm link
 $ROOT/$YALC_BIN_PATH publish
 cd $ROOT
 $ROOT/$YALC_BIN_PATH add $THEME_NAME
-yarn link $THEME_NAME
+npm link $THEME_NAME
